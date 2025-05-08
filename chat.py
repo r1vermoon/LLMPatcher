fdfe679d61c8cc3f1ab5508de506e54760f08b94
R1vermoon <24151213794@stu.xidian.edu.cn>
2025-05-08 15:12:50 +0800

    update

--- chat.py
+++ chat.py
@@ -1,23 +1,50 @@
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
+from transformers import Trainer,TrainingArguments
 import argparse
 import os
+
+from peft import LoraConfig, TaskType, get_peft_model
 
 from utils import load_datasets,chat
 
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com" 
 
+lora_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM, # type of task to train on
+    inference_mode=False, # set to False for training
+    r=8, # dimension of the smaller matrices
+    lora_alpha=32, # scaling factor
+    lora_dropout=0.1, # dropout of LoRA layers
+    target_modules=["q_proj", "v_proj"],
+)
+
 if __name__=="__main__":
     
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--model_name_or_pth', default='../../workdir/models/vicuna-13b-v1.5', help='Path to the model file or name of model')
+    parser.add_argument('--model_name_or_pth', default='../../workdir/models/Llama-2-7b-chat-hf', help='Path to the model file or name of model')
     parser.add_argument('--data_path', default="./data/UltraSafety/UltraSafety.jsonl", help='Path to the data')
     parser.add_argument('--device', default="auto", help='device')
     args = parser.parse_args()
     
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_pth)
     model = AutoModelForCausalLM.from_pretrained(args.model_name_or_pth,device_map=args.device)
-    prompt=load_datasets(args.data_path)
-    response = chat(prompt,model,tokenizer)
+    model = get_peft_model(model, lora_config) 
+    model.print_trainable_parameters()
     
-    print(response)
+    prompt=load_datasets(args.data_path,tokenizer)
+    
+    train_args= TrainingArguments(
+    output_dir="./output",  # 必须指定输出目录
+    per_device_train_batch_size=1  # 必须指定batch大小
+    )
+    
+    trainer = Trainer(
+        model=model,
+        args=train_args,
+        train_dataset=prompt # 你怎么就知道这个可以呢？
+    )
+    trainer.train()
+    
+    #response = chat(prompt,model,tokenizer)
+    # print(response)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+from datasets import Dataset
 
 def chat(prompt,model,tokenizer):
     input_ids=tokenizer.encode(prompt,return_tensors="pt").to(device="cuda")
@@ -10,12 +11,41 @@ def chat(prompt,model,tokenizer):
     response = tokenizer.decode(output[0], skip_special_tokens=True)
     return response
 
-def load_datasets(data_path):
+# def load_datasets(data_path):
+#     chat_prompts=[]
+#     with open(data_path, "r", encoding="utf-8") as f:
+#         for i, line in enumerate(f):
+#             if i >= 10:
+#                 break
+#             data = json.loads(line)
+#             chat_prompts.append(data['prompt'])
+#     return chat_prompts[9]
+
+def load_datasets(data_path,tokenizer):
     chat_prompts=[]
     with open(data_path, "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             if i >= 10:
                 break
             data = json.loads(line)
-            chat_prompts.append(data['prompt'])
-    return chat_prompts[9]
+            tokenizer.pad_token = "[PAD]"
+            encoded = tokenizer(
+                data["prompt"],
+                truncation=True,
+                padding="max_length",
+                max_length=512,
+                return_tensors="pt",
+                return_attention_mask=True,
+            )
+            
+            encoded["labels"] = tokenizer(
+                data["completions"][0]["response"],
+                truncation=True,
+                max_length=512,
+                padding="max_length",
+                return_tensors="pt"
+            ).input_ids
+            
+        chat_prompts.append({k: v.squeeze(0) for k, v in encoded.items()})
+    return Dataset.from_list(chat_prompts)
+
