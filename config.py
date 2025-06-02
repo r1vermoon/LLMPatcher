580233f5b4b35f80442d803e1aea042f9d25f852
R1vermoon <24151213794@stu.xidian.edu.cn>
2025-06-02 16:23:53 +0800

    update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--model_name_or_pth', default='../../workdir/models/vicuna-13b-v1.5', help='Path to the model file or name of model')
+    parser.add_argument('--model_name_or_pth', default='../../workdir/models/Llama-2-13b-chat-hf', help='Path to the model file or name of model')
     parser.add_argument('--data_path', default="./data/UltraSafety/UltraSafety.jsonl", help='Path to the data')
     parser.add_argument('--device', default="auto", help='device')
     parser.add_argument('--datasets_name', default='Prompts_Index', help='prompt datasets')
